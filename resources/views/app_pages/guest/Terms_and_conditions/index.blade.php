b192e93d75505f650c1b5de7dd2aeb5b7359bf91
Davyyd1 <d.dav889@gmail.com>
2024-03-01 00:19:37 +0200

    inceput navigatie

--- resources/views/app_pages/guest/Terms_and_conditions/index.blade.php
+++ resources/views/app_pages/guest/Terms_and_conditions/index.blade.php
@@ -14,7 +14,7 @@
         <div class="mx-auto max-w-7xl px-4 bg-[#161321] flex justify-center">
             <div class="py-9">
                 <div class="text-center text-white md:text-3xl text-xl font-semibold my-5"
-                    style="background-image: url(assets/img/png/optimizat.webp); width: 100%; background-size:cover; background-position:center; align-items:center; display:flex; justify-content:center; color:white;">
+                    style="background-image: url(assets/img/webp/header-img.webp); width: 100%; background-size:cover; background-position:center; align-items:center; display:flex; justify-content:center; color:white;">
                     {{-- <img src="assets/img/png/optimizat.webp" alt="Imagine"> --}}
                     {{ __('translations.terms_and_conditions') }}
                 </div>
